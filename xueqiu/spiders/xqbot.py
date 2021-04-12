fe73beec9434a2612d92d4d8d23e2d8a77957322
Haifei Cheng <haifei@berkeley.edu>
2021-04-11 22:18:59 -0700

    add multiple url support

--- xueqiu/spiders/xqbot.py
+++ xueqiu/spiders/xqbot.py
@@ -5,7 +5,9 @@ import json5
 class XqbotSpider(scrapy.Spider):
     name = 'xqbot'
     allowed_domains = ['www.xueqiu.com']
-    start_urls = ['https://xueqiu.com/P/ZH2126346']
+    start_urls = ['https://xueqiu.com/P/ZH2126346',
+                'https://xueqiu.com/P/ZH2268976',
+    ]
 
     def parse(self, response):
         # pass
@@ -20,10 +22,19 @@ class XqbotSpider(scrapy.Spider):
         pos_start = html.find('SNB.cubeInfo = ') + len('SNB.cubeInfo = ')
         pos_end = html.find('SNB.cubePieData')
         data = html[pos_start:pos_end]
+        #print(data)
         data = data.rstrip() # get rid of ending space
         data = data[:-1] # get rid of the last ";"
+
         dic = json5.loads(data)
+        #print('********************')
+        #name = dic['name']
+        #print(name.encode('utf-8'))
+        #print(dic['name'].encode('utf-8'))
+        #print('********************')
         scraped_data = {
+            'name' : dic['name'],
+            'symbol' : dic['symbol'],
             'daily_gain' : dic['daily_gain'],
             'monthly_gain' : dic['monthly_gain'],
             'total_gain' : dic['total_gain'],
